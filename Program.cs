a8b350a80155ccb2784ad4dcfa41fc30a66982e7
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-03-05 16:56:42 -0300

    Using Dijkstra? error tho

--- Program.cs
+++ Program.cs
@@ -9,13 +9,17 @@ var graph = BuildGraph();
 var start = graph.Nodes[3];
 var end = graph.Nodes[8];
 
-System.Console.WriteLine(start.Value.ToString());
-System.Console.WriteLine(end.Value.ToString());
+var weightedGraph = BuildWeightedGraph();
+var startw = weightedGraph.Nodes[1];
+var endw = weightedGraph.Nodes[5];
 
-var found = Search.BreadthFirstSearch
+System.Console.WriteLine(startw.Value.ToString());
+System.Console.WriteLine(endw.Value.ToString());
+
+var found = Search.Dijkstra
 (
-    new SearchGraphNode<string>(start),
-    end.Value
+    startw,
+    endw
 );
 
 System.Console.WriteLine(found ? "Goal Found" : "Goal Not Found");
@@ -74,7 +78,7 @@ Tree<int> BuildTree()
 
     return tree5;
 }
-Graph<string> BuildGraph()
+Graph<string, GraphNode<string>> BuildGraph()
 {
     var nodeBaltimore = new GraphNode<string>("Baltimore");
     var nodeBoston = new GraphNode<string>("Boston");
@@ -150,7 +154,7 @@ Graph<string> BuildGraph()
                 .AddNode(nodeNewYork)
                 .AddNode(nodePhiladelphia);
 
-    var buildGraph = new Graph<string>(new List<GraphNode<string>>
+    var buildGraph = new Graph<string, GraphNode<string>>(new List<GraphNode<string>>
     {
         nodeBaltimore,
         nodeBoston,
@@ -169,4 +173,100 @@ Graph<string> BuildGraph()
     });
 
     return buildGraph;
+}
+Graph<string, WeightedNode<string>> BuildWeightedGraph()
+{
+    var nodeBaltimore = new WeightedNode<string>("Baltimore");
+    var nodeBoston = new WeightedNode<string>("Boston");
+    var nodeBuffalo = new WeightedNode<string>("Buffalo");
+    var nodeChicago = new WeightedNode<string>("Chicago");
+    var nodeCleveland = new WeightedNode<string>("Cleveland");
+    var nodeColumbus = new WeightedNode<string>("Columbus");
+    var nodeDetroit = new WeightedNode<string>("Detroit");
+    var nodeIndianapolis = new WeightedNode<string>("Indianapolis");
+    var nodeNewYork = new WeightedNode<string>("New York");
+    var nodePhiladelphia = new WeightedNode<string>("Philadelphia");
+    var nodePittsburgh = new WeightedNode<string>("Pittsburgh");
+    var nodePortland = new WeightedNode<string>("Portland");
+    var nodeProvidence = new WeightedNode<string>("Providence");
+    var nodeSyracuse = new WeightedNode<string>("Syracuse");
+
+    nodeBaltimore.AddNode(nodePhiladelphia, 101)
+                 .AddNode(nodePittsburgh, 247);
+    
+    nodeBoston.AddNode(nodeNewYork, 215)
+              .AddNode(nodePortland, 107)
+              .AddNode(nodeProvidence, 50)
+              .AddNode(nodeSyracuse, 312);
+    
+    nodeBuffalo.AddNode(nodeCleveland, 189)
+               .AddNode(nodeDetroit, 256)
+               .AddNode(nodePittsburgh, 215)
+               .AddNode(nodeSyracuse, 150);
+    
+    nodeChicago.AddNode(nodeCleveland, 345)
+               .AddNode(nodeDetroit, 283)
+               .AddNode(nodeIndianapolis, 182);
+    
+    nodeCleveland.AddNode(nodeBuffalo, 189)
+                 .AddNode(nodeChicago, 345)
+                 .AddNode(nodeColumbus, 144)
+                 .AddNode(nodeDetroit, 169)
+                 .AddNode(nodePittsburgh, 134);
+    
+    nodeColumbus.AddNode(nodeCleveland, 144)
+                .AddNode(nodeIndianapolis, 176)
+                .AddNode(nodePittsburgh, 185);
+    
+    nodeDetroit.AddNode(nodeBuffalo, 256)
+               .AddNode(nodeChicago, 283)
+               .AddNode(nodeCleveland, 169);
+    
+    nodeIndianapolis.AddNode(nodeChicago, 182)
+                    .AddNode(nodeColumbus, 176);
+    
+    nodeNewYork.AddNode(nodeBoston, 215)
+               .AddNode(nodePhiladelphia, 97)
+               .AddNode(nodeProvidence, 181)
+               .AddNode(nodeSyracuse, 254);
+    
+    nodePhiladelphia.AddNode(nodeBaltimore, 101)
+                    .AddNode(nodeNewYork, 97)
+                    .AddNode(nodeSyracuse, 253);
+    
+    nodePittsburgh.AddNode(nodeBaltimore, 247)
+                  .AddNode(nodeBuffalo, 215)
+                  .AddNode(nodeCleveland, 134)
+                  .AddNode(nodeColumbus, 185)
+                  .AddNode(nodePhiladelphia, 305);
+    
+    nodePortland.AddNode(nodeBoston, 107);
+    
+    nodeProvidence.AddNode(nodeBoston, 50)
+                  .AddNode(nodeNewYork, 181);
+    
+    nodeSyracuse.AddNode(nodeBoston, 312)
+                .AddNode(nodeBuffalo, 150)
+                .AddNode(nodeNewYork, 254)
+                .AddNode(nodePhiladelphia, 253);
+
+    var buildWeightedGraph = new Graph<string, WeightedNode<string>>(new List<WeightedNode<string>>
+    {
+        nodeBaltimore,
+        nodeBoston,
+        nodeBuffalo,
+        nodeChicago,
+        nodeCleveland,
+        nodeColumbus,
+        nodeDetroit,
+        nodeIndianapolis,
+        nodeNewYork,
+        nodePhiladelphia,
+        nodePittsburgh,
+        nodePortland,
+        nodeProvidence,
+        nodeSyracuse,
+    });
+
+    return buildWeightedGraph;
 }
